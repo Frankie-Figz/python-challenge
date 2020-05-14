b28e53d7343ad9dd71a835219a4be7861f953917
Andrew Addie <theaddies@neo.rr.com>
2020-05-14 11:04:00 -0400

    Code works but it is messy with integers for data

---
+++ PyBank/main.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 20:46:08 2020
+
+@author: theaddies
+"""
+
+# Module for reading CSV files
+import csv
+
+financeData=[]
+
+csvpath = './Resources/budget_data.csv'
+
+# Improved Reading using CSV module
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+    financeData.append(csv_header)
+    
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+        financeData.append(row)
+        
+n = 0
+maxProfitIncrease = 0
+maxProfitDecrease = 0
+totalProfit = 0
+profitChange = 0
+totalProfitChange = 0
+
+print(financeData)
+
+for row in range(1, len(financeData)):
+    financeData[int(row)][1] = int(financeData[int(row)][1])
+
+for row in range(1, len(financeData)):
+   totalProfit = totalProfit + financeData[int(row)][1]
+   if row <= (len(financeData)-2):
+       profitChange = financeData[int(row+1)][1] - financeData[int(row)][1]
+       totalProfitChange = totalProfitChange + profitChange
+   if maxProfitIncrease< profitChange:
+        maxProfitIncrease = profitChange
+        increaseDate = int(row)
+   if maxProfitDecrease > profitChange:
+        maxProfitDecrease = profitChange
+        decreaseDate = int(row)
+averageChange = totalProfitChange / (len(financeData)-2)
+        
+print("Financial Analysis")
+print("--------------------------------")
+
+print(f"Total months: = {str(len(financeData)-1)}")
+print(f"Total: = {totalProfit}")
+print(f"Average Change: = {str(averageChange)}")
+print(f"Greatest Incresae in Profits: {financeData[increaseDate][0]} {maxProfitIncrease}")
+print(f"Greatest Incresae in Profits: {financeData[decreaseDate][0]} {maxProfitDecrease}")
+      
