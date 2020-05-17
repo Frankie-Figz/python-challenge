451d799d4b70965643cf9503306325f08d13e7db
Andrew Addie <theaddies@neo.rr.com>
2020-05-17 08:35:16 -0400

    calculated percentage and votes need dictionary ranked

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,11 +8,16 @@ Created on Wed May 13 20:46:08 2020
 # Module for reading CSV files
 import csv
 
+def printScreenFile(message):
+    print(message)
+    writer.writerow([message])
+
 voterCast=[]
 
 voterTally = {}
 
 csvpath = './Resources/election_data.csv'
+output_file = './analysis/election_results.csv'
 
 # Improved Reading using CSV module
 
@@ -41,11 +46,19 @@ filteredCandidates = set(voterCast)
 #I know there is a built in function "from collections import counter" that would do what I need.  Wanted to learn more.
 
 for candidate in filteredCandidates:
-     voterTally[candidate] = 0
+     voterTally[candidate] = [0,0]
     
-for vote in voterCast:
-    for candidate in filteredCandidates:
-#        voterTally[candidate] = 0
+for candidate in filteredCandidates:
+    for vote in voterCast:
         if candidate == vote:
-         voterTally[candidate] = voterTally[candidate] + 1
-         
+         voterTally[candidate][1] = int(voterTally[candidate][1]) + 1
+    voterTally[candidate][0] = float(100 * voterTally[candidate][1] / totalVotes)
+#for candidate in voterTally:
+    
+
+# with open(output_file, "w", newline='') as datafile:
+#     writer = csv.writer(datafile, delimiter=' ')
+#     printScreenFile("Election Results")
+#     printScreenFile("--------------------------------")
+#     printScreenFile(f"Total Votes: {totalVotes}")
+#     print
